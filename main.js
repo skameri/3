734cb92ac47dfe63565cb9373e7c09e4b70c5875
Papuna Tsereteli <117572811+skameri@users.noreply.github.com>
2023-01-23 16:52:39 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,63 @@
-const toDoList = []; //მასივის შემოტანა
-const tittleTodoList = []; //კიდევ მასივი
-const $input = document.getElementsByTagName("input")[0];
-const $button = document.getElementsByTagName("button")[0];
-const $ul = document.getElementsByTagName("ul")[0];
+const toDoList = [] //ლისტი ელემენტისთვის
 
-var id = toDoList.length + 1; // ათვლა რომ დაიწყოს ერთიდან
 
-$button.addEventListener("click", addTodoItem); //ევენთლისენერით ენიჭება ღილაკს ქლიქ ფუნქცია
+const $input = document.getElementsByTagName('input')[0]
+const $button = document.getElementsByTagName('button')[0]
+const $ul = document.getElementsByTagName('ul')[0]
+
+var id = toDoList.length + 1 //რომ დაიწყოს აიდი ერთიდან
+
+
+$button.addEventListener('click', addTodoItem) // ონკლიკ ფუნქცია
 
 function addTodoItem() {
-  //დამატების ფუნქცია
-
-  let newInput = $input.value; //ცვლადში ინახავს ინფუთის ველიუს
-
-  if (validateInput(newInput)) {
-    //
-    if (!tittleTodoList.includes($input.value)) {
-      //მასივი, რომელიც შედგება ინფუთ ველიუსგან
-      let newItem = new Object(); // ობიექტის დამატება
-      newItem.id = id; //ახალი აიდი
-      id++; // აიდის მომატება
-      newItem.title = $input.value; //თაითლს უტოლებს  ველიუს
-      toDoList.push(newItem); // ახალი აითემის დამატება
-      tittleTodoList.push(newItem.title); //ახალი თაითლის დამატება
-      $input.value = null; //ინფუთ ველიუს ადგილი როცა ცარიელია
-      renderTodoList(); //რენდერი
-      console.log(tittleTodoList); //დალოგილია თაითლთუდულისტი
+    let newInput = $input.value //ველიუს ვინახავთ 
+    if (validateInput(newInput)) { // ახალ მნიშვნელობაზე იძახებს ფუნქციას
+        let newItem = new Object() // ახალი ობიექტი
+        newItem.id = id 
+        id++ // იზრდება აიდი
+        newItem.title = $input.value //ვამათებს ახალ აითემს 
+        toDoList.push(newItem) // ვამატებთ აითემს თოდოს
+        $input.value = null // ველს ვასუფთავებთ
+        renderTodoList() //რენდერი
     } else {
-      console.log("its similar element"); //თუ მსგავსი ელემენტია ამას გამოიტანს კონსოლზე
+        console.log('The input must be filled') // ერორის ტექსტი
     }
-  } else {
-    console.log("The input must be filled"); //თუ არაფერს ჩაწერ ამას გამოიტანს კონსოლზე
 }
 
-function deleteTodoItem(id) {
-  // წაშლის ფუნქცია
-  deleteTodoItemFromtoDoList(id);
-  renderTodoList();
+function deleteTodoItem(id) { // წაშლის ფუნქცია
+    deleteTodoItemFromtoDoList(id) // ვიძახებთ ფუნქციას
+    renderTodoList() // შედეგის გამოტანა
 }
 
-function renderTodoList() {
-  $ul.innerHTML = null; //ული როცა ცარიელია
+function renderTodoList() { // დარენდერების შედეგი
 
-  toDoList.forEach((item) => {
+    $ul.innerHTML = null // გაასუფთაოს <UL> html გამოსახულება 
 
-    let li = document.createElement("li"); //ახალი ელემენტის შექმნა
-    li.innerHTML = item.title + " ID: " + item.id + " - "; //ლისთვის აიდის და თაითლის გაწერა
-    let delButton = document.createElement("button"); // წაშლის ღილაკი
-    delButton.innerHTML = "Delete"; //სახელის დაწერს ღილაკზე
-
-    delButton.addEventListener("click", () => {
-      //კლიკ ფუნქცია
-      deleteTodoItem(item.id); //აიდის წაშლა
-    });
-    li.appendChild(delButton);
-    $ul.appendChild(li);
-  });
+    toDoList.forEach(item => {
+        let li = document.createElement('li') // შეიქმნას ლი
+        li.innerHTML = item.title + " ID: " + item.id + " - " // გამოსატანი ტექსტი
+        let delButton = document.createElement('button') // წაშლის ღილაკი
+        delButton.innerHTML = "Delete" // აწერს ღილაკს delete ს
+        delButton.addEventListener('click', () => { // კლიკის დროს რა უნდა ქნას
+            deleteTodoItem(item.id) // აიდით მიაგნოს და წაშალოს მნიშვნელობა
+        })
+        li.appendChild(delButton) // წაშლის ღილაკი
+        $ul.appendChild(li) // ლის მშობელი
+    }
+    )
 }
 
-function deleteTodoItemFromtoDoList(id) {
-  //ამოშლის ფუნქცია
-  let item = toDoList.find((value) => value.id === id); // აიდით მოძებნა
-  toDoList.splice(toDoList.indexOf(item), 1);
+function deleteTodoItemFromtoDoList(id) { // წაშლის ფუნქიონარი
+    let item = toDoList.find(value => value.id === id) // აიდის მოძებნა
+    toDoList.splice(toDoList.indexOf(item), 1) // წაშლა
 }
 
-function validateInput(value) {
-  //ვალიუს განსზაღვრა
-  if (value !== "") {
-    // არგუმენტი არ უნდა იყოს ცარიელი
-    return true;
-    // თუ არგუმეტი ცარიელი არაა აბრუნებს თრუს
-  } else {
-    return false;
-    // თუ ცარიელია აბრუნებსს ფოლსს
-  }
+function validateInput(value) { //თუ არაფერს ჩავწერთ, გამოიძახოს ეს ფუნქცია
+    if(value !== ''){ // თუ რამე წერია აბრუნებს თრუს
+        return true
+    }else{
+        return false // თუ არაფერი წერია აბრუნებს ფოლსს
+    }
 }
-
-renderTodoList()
+renderTodoList() 
